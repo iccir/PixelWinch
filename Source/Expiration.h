58bb9e3007db9d15b6d03922a02c8923365edefc
Ricci Adams <git@ricciadams.com>
2014-05-28 02:21:34 -0700

    Trying to fix code signing

--- Source/Expiration.h
+++ Source/Expiration.h
@@ -1,3 +1,3 @@
-#define kExpirationDouble ((double)    428112000.0 )
-#define kExpirationFloat  ((float)     428112000.0f)
-#define kExpirationLong   ((long long) 428112000   )
+#define kExpirationDouble ((double)    430704000.0 )
+#define kExpirationFloat  ((float)     430704000.0f)
+#define kExpirationLong   ((long long) 430704000   )
