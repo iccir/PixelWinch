caa6dcd2bd5cb49881e48ebc0956b044e957da09
Ricci Adams <git@ricciadams.com>
2014-05-28 17:06:26 -0700

    Archiving Pixel Winch-210

--- Source/Expiration.h
+++ Source/Expiration.h
@@ -1,3 +1,3 @@
-#define kExpirationDouble ((double)    430704000.0 )
-#define kExpirationFloat  ((float)     430704000.0f)
-#define kExpirationLong   ((long long) 430704000   )
+#define kExpirationDouble ((double)    430790400.0 )
+#define kExpirationFloat  ((float)     430790400.0f)
+#define kExpirationLong   ((long long) 430790400   )
