b0213d046ac129b5c2530eb241befcb694f8dd69
Ricci Adams <git@ricciadams.com>
2016-08-06 18:43:38 -0700

    Version to 1.1.1

--- Source/BetaExpiration.h
+++ Source/BetaExpiration.h
@@ -1,4 +1,4 @@
-// Expires: 2016-08-06
+// Expires: 2016-09-05
 
-#define kExpirationDouble ((double)    492165678.0 )
-#define kExpirationLong   ((long long) 492165678   )
+#define kExpirationDouble ((double)    494818919.0 )
+#define kExpirationLong   ((long long) 494818919   )
