2de00ad84ec69663ee0e80a43b485dc30ea9756d
Ricci Adams <git@ricciadams.com>
2016-11-07 01:32:37 -0800

    Bump beta

--- Source/BetaExpiration.h
+++ Source/BetaExpiration.h
@@ -1,4 +1,4 @@
-// Expires: 2016-12-06
+// Expires: 2016-12-07
 
-#define kExpirationDouble ((double)    502771214.0 )
-#define kExpirationLong   ((long long) 502771214   )
+#define kExpirationDouble ((double)    502795940.0 )
+#define kExpirationLong   ((long long) 502795940   )
