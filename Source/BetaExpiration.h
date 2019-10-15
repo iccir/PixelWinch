0461f81783f323b20930aa6d98b37696a4ecc759
Ricci Adams <git@ricciadams.com>
2019-10-15 04:01:06 -0700

    Beta expiration

--- Source/BetaExpiration.h
+++ Source/BetaExpiration.h
@@ -1,4 +1,4 @@
-// Expires: 2018-03-26
+// Expires: 2019-11-14
 
-#define kExpirationDouble ((double)    512269221.0 )
-#define kExpirationLong   ((long long) 512269221   )
+#define kExpirationDouble ((double)    595425655.0 )
+#define kExpirationLong   ((long long) 595425655   )
