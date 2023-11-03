7f7aea7f8f8993a461647ec00377292e41b5b57d
Ricci Adams <git@ricciadams.com>
2023-11-03 14:39:30 -0700

    altool -> notarytool

--- Build/Archive.sh
+++ Build/Archive.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
+BUILD_PREFIX="PixelWinch"
+
 TEAM_ID="XXXXXXXXXX"
-
-APP_NAME="PixelWinch"
-
-NOTARY_BUNDLE_ID="com.iccir.PixelWinch"
-NOTARY_APPLE_ID="<redacted>"
-NOTARY_ASC_PROVIDER="RicciAdams1115211120"
-NOTARY_PASSWORD="<redacted>"
+KEYCHAIN_PROFILE="<redacted>"
 
 ZIP_TO="$HOME/Desktop"
 UPLOAD_TO="<redacted>"
-BUILD_STRING=""
+PUBLIC_URL="https://www.ricciadams.com/downloads/pixel-winch/betas"
 
 # ----------------------------------
+
+BUILD_STRING=""
 
 show_notification ()
 {
@@ -47,7 +45,7 @@ get_plist_build ()
     printf $(defaults read "$1" CFBundleVersion | sed 's/\s//g' )
 }
 
-TMP_DIR=`mktemp -d /tmp/${APP_NAME}-Archive.XXXXXX`
+TMP_DIR=$(mktemp -d /tmp/"${BUILD_PREFIX}"-Archive.XXXXXX)
 STATUS_MD="${TMP_DIR}/status.md"
 
 # 1. Export archive to tmp location and set APP_FILE, push to parent directory
@@ -62,7 +60,8 @@ xcodebuild -exportArchive -archivePath "${ARCHIVE_PATH}" -exportOptionsPlist "${
 APP_FILE=$(find "${TMP_DIR}" -name "$FULL_PRODUCT_NAME" | head -1)
 
 BUILD_NUMBER=$(get_plist_build "$APP_FILE"/Contents/Info.plist)
-BUILD_STRING="${APP_NAME}-${BUILD_NUMBER}"
+BUILD_STRING="${BUILD_PREFIX}-${BUILD_NUMBER}"
+ZIP_FILE="${BUILD_STRING}.zip"
 
 add_log "ARCHIVE_PATH = '$ARCHIVE_PATH'"
 add_log "FULL_PRODUCT_NAME = '$FULL_PRODUCT_NAME'"
@@ -76,79 +75,50 @@ open -b com.apple.dt.Xcode "$STATUS_MD"
 pushd "$APP_FILE"/.. > /dev/null
 
 
-# 2. Zip up $APP_FILE to "App.zip" and upload to notarization server
+# Zip up $APP_FILE to $ZIP_FILE and upload to notarization server
 
-zip --symlinks -r App.zip "$(basename "$APP_FILE")"
+zip --symlinks -r "$ZIP_FILE" $(basename "$APP_FILE")
 
 set_status "Sending to Apple notary service. This may take several minutes."
 
-xcrun altool \
-    --notarize-app --file App.zip --type osx \
-    --primary-bundle-id "$NOTARY_BUNDLE_ID" \
-    --username "$NOTARY_APPLE_ID" \
-    --password "$NOTARY_PASSWORD" \
-    --asc-provider "$NOTARY_ASC_PROVIDER" \
-    > "${TMP_DIR}/output-notarize-app.txt" 2>&1 
+xcrun notarytool submit "$ZIP_FILE" \
+    --keychain-profile "$KEYCHAIN_PROFILE" \
+    --output-format plist \
+    --wait \
+    1> "${TMP_DIR}/output-notarytool-submit.plist" \
+    2> "${TMP_DIR}/output-notarytool-submit-err.txt"
 
-NOTARY_UUID=$(grep RequestUUID "${TMP_DIR}/output-notarize-app.txt" | awk '{print $3}')
+SUBMIT_ID=$(    defaults read "${TMP_DIR}/output-notarytool-submit.plist" id)
+SUBMIT_STATUS=$(defaults read "${TMP_DIR}/output-notarytool-submit.plist" status)
+ 
+add_log "SUBMIT_ID = '$SUBMIT_ID'"
+add_log "SUBMIT_STATUS = '$SUBMIT_STATUS'"
 
-add_log "NOTARY_UUID = '$NOTARY_UUID'"
-
-# 3. Wait for notarization
-
-NOTARY_SUCCESS=0
-
-set_status "Finished sending to Apple notary service"
-
-while true
-do
-    NOTARY_OUTPUT=$(
-        xcrun altool \
-        --notarization-info "${NOTARY_UUID}" \
-        --username "$NOTARY_APPLE_ID" \
-        --password "$NOTARY_PASSWORD" \
-        2>&1
-    )
-
-    if [ $? -ne 0 ]; then
-        add_log "altool --notarization-info returned $?"
-    fi
-
-    add_log "${NOTARY_OUTPUT}"
-    set_status "Waiting for notary response." "${NOTARY_OUTPUT}"
-    
-    if [[ "${NOTARY_OUTPUT}" =~ "Invalid" ]] ; then
-        add_log "altool --notarization-info results invalid"
-        break
-    fi
-
-    if [[ "${NOTARY_OUTPUT}" =~ "success" ]]; then
-        NOTARY_SUCCESS=1
-        break
-    fi
-
-    sleep 2
-done
-
-
-# 4. Staple
-
-if [ $NOTARY_SUCCESS -eq 1 ] ; then
+if [[ "${SUBMIT_STATUS}" =~ "Accepted" ]] ; then
     set_status "Stapling file."
     xcrun stapler staple "$APP_FILE"
-    
-else
-    set_status "Error during notarization."
-fi
 
-# 5. Re-zip file and upload
-
-if [ $NOTARY_SUCCESS -eq 1 ] ; then
-    FINAL_ZIP_FILE="$ZIP_TO/$BUILD_STRING".zip
-    zip --symlinks -r "$FINAL_ZIP_FILE" "$(basename "$APP_FILE")"
+    FINAL_ZIP_FILE="$ZIP_TO/$ZIP_FILE"
+    zip --symlinks -r "$FINAL_ZIP_FILE" $(basename "$APP_FILE")
     scp "$FINAL_ZIP_FILE" "$UPLOAD_TO"
 
-    set_status "Uploaded '$BUILD_STRING.zip' to server. **confetti**"
+    if [ -n "$PUBLIC_URL" ]; then
+        set_status "Uploaded '$ZIP_FILE' to server." "$PUBLIC_URL/$ZIP_FILE"
+    else
+        set_status "Uploaded '$ZIP_FILE' to server. **confetti**"
+    fi
+
+else 
+    set_status "Received status of ${SUBMIT_STATUS}.\n\nFetching log."
+
+    xcrun notarytool log "$SUBMIT_ID" \
+        --keychain-profile "$KEYCHAIN_PROFILE" \
+        1> "${TMP_DIR}/output-notarytool-log.txt" \
+        2> "${TMP_DIR}/output-notarytool-log-err.txt"
+
+    NOTARY_LOG=$(cat "${TMP_DIR}/output-notarytool-log.txt")
+    
+    set_status "Received status of ${SUBMIT_STATUS}." "$NOTARY_LOG"
 fi
 
 popd > /dev/null
